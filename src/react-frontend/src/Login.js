0cdbf665932a6f71d77541cbf22d4cba6f7efa5c
Tal Gordon <nuvenin@proton.me>
2025-01-20 19:14:40 +0200

    unauthenticated users homepage

---
+++ src/react-frontend/src/Login.js
@@ -0,0 +1,67 @@
+import './Login.css';
+import { useState } from 'react';
+
+function Login() {
+	const [isAuthenticated, setIsAuthenticated] = useState(false);
+	const [username, setUsername] = useState('');
+	const [password, setPassword] = useState('');
+	const [errorMessage, setErrorMessage] = useState('');
+
+	const handleLogin = async () => {
+    const authRequestOptions = {
+    	method: 'POST',
+    	headers: { 'Content-Type': 'application/json' },
+    	body: JSON.stringify({ username: username, password: password })
+    };
+    try {
+        const authResponse = await fetch('/tokens', authRequestOptions);
+        if (authResponse.ok) {
+          // User is authenticated
+        	setIsAuthenticated(true);
+        } else {
+        // User is not authenticated
+        	setIsAuthenticated(false);
+            const errorText = await authResponse.text(); // Get error message from server
+        	const errorObject = JSON.parse(errorText);
+        	const serverErrorMessage = errorObject.message || errorObject.error || errorObject.description || errorMessage; 
+        	setErrorMessage(serverErrorMessage);
+        }
+    } catch(error) {
+    	setIsAuthenticated(false);
+    	setErrorMessage(error || 'An error occurred during login.');
+    };
+};
+
+return (
+    <div className="auth-container">
+	{isAuthenticated ? (
+        <h1 className="welcome-message">Welcome, User!</h1>
+	) : (
+        <div className="login-form">
+		<h1 className="login-title">Welcome back. <br/>You have been missed.</h1>
+		<input
+            type="text"
+            className="login-input"
+            placeholder="Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+		/>
+		<input
+            type="password"
+            className="login-input"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+		/>
+		<button className="login-button" onClick={handleLogin}>
+            Login
+		</button>
+          {/* TODO: need an account? */}
+		{errorMessage && <div className="error-message">{errorMessage}</div>}
+        </div>
+	)}
+    </div>
+	);  
+}
+
+export default Login;
---
+++ src/react-frontend/src/home.unauth.js
@@ -0,0 +1,35 @@
+import './home.unauth.css';
+import Login from './Login';
+import { BrowserRouter, Routes, Route, Link } from "react-router";
+
+function HomeUnauth() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/login" element={<Login />} />
+            </Routes>
+            <body>
+                <nav className="navbar">
+                    <ul className="nav-list">
+                        <li className="nav-item">
+                            <a className="nav-link" href="/">Home</a>
+                        </li>
+                    </ul>
+                    <div>
+                        <Link to="/login">
+                            <button className="login-button-navbar">Login</button>
+                        </Link>
+                        <Link to="/signup">
+                            <button className="login-button-navbar">Sign Up</button>
+                        </Link>
+                    </div>
+                </nav>
+                <div className="home-container">
+                    <h1 className="welcome-message-home">We've been waiting for you. Are you ready?</h1>
+                </div>
+            </body>
+        </BrowserRouter>
+        ); 
+}
+
+export default HomeUnauth;
