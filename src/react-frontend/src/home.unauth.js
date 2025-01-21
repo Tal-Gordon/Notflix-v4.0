342ec84e4e6653d60d9d10e5934dfcba93af31ee
Yoav Cohen <yoavi.cohen100@gmail.com>
2025-01-21 16:31:40 +0200

    moved routes to index

--- src/react-frontend/src/home.unauth.js
+++ src/react-frontend/src/home.unauth.js
@@ -1,37 +1,29 @@
 import './home.unauth.css';
-import Login from './Login';
-import HomeAuth from './home.auth';
-import { BrowserRouter, Routes, Route, Link } from "react-router";
+import { Link } from 'react-router-dom';
 
 function HomeUnauth() {
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/login" element={<Login />} />
-                <Route path="/browse" element={<HomeAuth />} /> 
-            </Routes>
-            <body>
-                <nav className="navbar">
-                    <ul className="nav-list">
-                        <li className="nav-item">
-                            <a className="nav-link" href="/">Home</a>
-                        </li>
-                    </ul>
-                    <div>
-                        <Link to="/login">
-                            <button className="login-button-navbar">Login</button>
-                        </Link>
-                        <Link to="/signup">
-                            <button className="login-button-navbar">Sign Up</button>
-                        </Link>
-                    </div>
-                </nav>
-                <div className="home-container">
-                    <h1 className="welcome-message-home">We've been waiting for you. Are you ready?</h1>
+        <>
+            <nav className="navbar">
+                <ul className="nav-list">
+                    <li className="nav-item">
+                        <a className="nav-link" href="/">Home</a>
+                    </li>
+                </ul>
+                <div>
+                    <Link to="/login">
+                        <button className="login-button-navbar">Login</button>
+                    </Link>
+                    <Link to="/signup">
+                        <button className="login-button-navbar">Sign Up</button>
+                    </Link>
                 </div>
-            </body>
-        </BrowserRouter>
-        ); 
+            </nav>
+            <div className="home-container">
+                <h1 className="welcome-message-home">We've been waiting for you. Are you ready?</h1>
+            </div>
+        </>
+    );
 }
 
 export default HomeUnauth;
--- src/react-frontend/src/index.js
+++ src/react-frontend/src/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './index.css';
+import HomeAuth from './home.auth';
 import HomeUnauth from './home.unauth';
+import Login from './Login';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-	<React.StrictMode>
-		<HomeUnauth />
-	</React.StrictMode>
+  <React.StrictMode>
+    <Router>
+      <Routes>
+        {/* Define all routes */}
+        <Route path="/" element={<HomeUnauth />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/browse" element={<HomeAuth />} />
+      </Routes>
+    </Router>
+  </React.StrictMode>
 );
